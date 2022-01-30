8ac63ff6cd7705a577ad4f4fbbc2648a31575b02
danielyoungkimball <danielyoungkimball@outlook.com>
2022-01-29 16:09:50 -0800

    bro idek tbh

---
+++ README.md
@@ -0,0 +1,1 @@
+Made by Daniel Young Kimball
---
+++ src/user.js
@@ -0,0 +1,10 @@
+export default function User() {
+
+  handleLeft(() => {
+    console.log('left')
+  });
+
+  return (
+    <div className="UserBody">hi</div>
+  );
+}
