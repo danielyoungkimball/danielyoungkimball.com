3836c9b975e77ebd9144ffc2217e57d9e2c04f82
Daniel Kimball <danielyoungkimball@outlook.com>
2023-03-07 01:46:20 -0800

    yes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './styles.css';
 import PDFViewer from './PDFViewer';
 
 function App() {
-    return (
-        <div className="container">
-            <div className="leftColumn">
-                <img src="/profileImg.png" alt="Profile" className="profile-img" />
+  return (
+    <div className="container">
+      <div className="leftColumn">
+        <div className="centered">
+          <div className="profile-img-container">
+            <img src="/profileImg.png" alt="Profile" className="profile-img" />
+            <div className="hover-card">
+              <span>Milan, Italy 📍</span>
             </div>
-            <div className="rightColumn">
-                <PDFViewer />
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="rightColumn">
+        <PDFViewer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -5,22 +5,15 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function PDFViewer() {
-  const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
 
   return (
     <div>
-      <Document file="/path/to/your/file.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+      <Document file="/kimball-resume.pdf">
+        <Page pageNumber={pageNumber} renderTextLayer={false}/>
       </Document>
-      <p>Page {pageNumber} of {numPages}</p>
-      <button disabled={pageNumber <= 1} onClick={() => setPageNumber(pageNumber - 1)}>Previous</button>
-      <button disabled={pageNumber >= numPages} onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
-      <a href="/path/to/your/file.pdf" download>Download</a>
+      <a href="/kimball-resume.pdf" download>Download</a>
     </div>
   );
 }
