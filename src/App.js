c7519dfd8eded677229d94cf4177ff3f8c734669
Daniel Kimball <danielyoungkimball@outlook.com>
2023-03-28 15:42:23 -0700

    mv1

--- src/App.js
+++ src/App.js
@@ -1,48 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import './styles.css';
+import './header.css';
+import './skills.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function App() {
+function Header() {
   return (
-    <div className="container">
-      <header className="header">
+    <header className="header card">
+      <div className="column">
         <div className="profile-card">
           <img src='/profileImg.png' alt="Profile" className="profile-picture" />
-          <div className="header-info">
-            <h1 className="name">John Doe</h1>
-            <h2 className="job-title">Senior Software Engineer</h2>
-            <div className="social-links">
-              <a href="https://www.linkedin.com/in/your_username" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-              <a href="https://github.com/your_username" target="_blank" rel="noopener noreferrer">GitHub</a>
-            </div>
-          </div>
-          <div className="card-left">
-            <p>Milan, Italy</p>
-          </div>
         </div>
-      </header>
-
-      <div className="main-content">
-        <section className="card about">
-          <h3>About Me</h3>
-          <p>I am a highly skilled senior software engineer with over 10 years of experience. Specializing in modern web technologies, I am passionate about creating efficient, scalable, and elegant solutions.</p>
-        </section>
-
-        <section className="card skills">
-          <h3>Skills</h3>
-          <ul>
-            <li>React</li>
-            <li>JavaScript</li>
-            <li>Node.js</li>
-            <li>Python</li>
-            <li>Java</li>
-            <li>Git</li>
-          </ul>
-        </section>
       </div>
+      <div className="column">
+        <div className="header-info">
+          <h1 className="name">Daniel Kimball</h1>
+          <h2 className="job-title">Senior Software Engineer</h2>
+        </div>
+      </div>
+      <div className="column">
+        <div className="header-links">
+          <a href="https://www.linkedin.com/in/danielyoungkimball/" target="_blank" rel="noopener noreferrer">
+            <img src="/linkedin.png" alt="LinkedIn" className="header-link" />
+          </a>
+          <a href="https://www.github.com/danielyoungkimball/" target="_blank" rel="noopener noreferrer">
+            <img src="/github.png" alt="GitHub" className="header-link" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
 
-      <footer className="footer">
-        <p>Contact: johndoe@example.com</p>
-      </footer>
+function PDFViewer() {
+  const [pageNumber] = useState(1);
+  const pageStyle = {
+    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.1)",
+  };
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Document file="/kimball-resume.pdf">
+        <Page pageNumber={pageNumber} renderTextLayer={false} style={pageStyle} />
+      </Document>
+    </div>
+  );
+}
+
+
+
+function Resume() {
+  return (
+    <section className="card">
+      <div className='title'>
+        <h3>Resume</h3>
+        <a href="/kimball-resume.pdf" download>Download</a>
+      </div>
+      <PDFViewer />
+    </section>
+  );
+}
+
+function Skills() {
+  return (
+    <section className="card skills">
+      <h3>Skills</h3>
+      <div className="skills-section">
+        <h4>Programming Languages:</h4>
+        <ul>
+          <li className="skill">JavaScript</li>
+          <li className="skill">Python</li>
+          <li className="skill">Java</li>
+          <li className="skill">C++</li>
+        </ul>
+      </div>
+      <div className="skills-section">
+        <h4>Web Technologies:</h4>
+        <ul>
+          <li className="skill">HTML5</li>
+          <li className="skill">CSS3</li>
+          <li className="skill">React.js</li>
+          <li className="skill">GraphQL</li>
+          <li className="skill">Angular</li>
+          <li className="skill">Vue.js</li>
+        </ul>
+      </div>
+      <div className="skills-section">
+        <h4>Backend Technologies:</h4>
+        <ul>
+          <li className="skill">Node.js</li>
+          <li className="skill">Express.js</li>
+          <li className="skill">Django</li>
+          <li className="skill">Ruby on Rails</li>
+        </ul>
+      </div>
+      <div className="skills-section">
+        <h4>Databases:</h4>
+        <ul>
+          <li className="skill">SQL</li>
+          <li className="skill">MongoDB</li>
+          <li className="skill">Redis</li>
+        </ul>
+      </div>
+      <div className="skills-section">
+        <h4>Tools &amp; Technologies:</h4>
+        <ul>
+          <li className="skill">Git</li>
+          <li className="skill">Docker</li>
+          <li className="skill">Kubernetes</li>
+          <li className="skill">Azure</li>
+          <li className="skill">AWS</li>
+          <li className="skill">Google Cloud</li>
+        </ul>
+      </div>
+      <div className="skills-section">
+        <h4>Soft Skills:</h4>
+        <ul>
+          <li className="skill">Agile Development</li>
+          <li className="skill">Team Leadership</li>
+          <li className="skill">Strong Communication</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Contact: danielyoungkimball@outlook.com</p>
+    </footer>
+  );
+}
+
+export default function App() {
+  const [isResumeVisible, setIsResumeVisible] = useState(false);
+
+  const toggleResumeVisibility = () => {
+    setIsResumeVisible(!isResumeVisible);
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      <Resume />
+      <Skills />
+      <Footer />
     </div>
   );
 };
